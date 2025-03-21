f98340280d74ecc29577d0c619ccb33f5dc24dcc
Shaun van Zitters <triads_torsion.0y@icloud.com>
2025-03-21 11:44:03 +0200

    Initial Commit

---
+++ WeatherCastTests/WeatherCastTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherCastTests.swift
+//  WeatherCastTests
+//
+//  Created by Shaun van Zitters on 2025/03/21.
+//
+
+import Testing
+@testable import WeatherCast
+
+struct WeatherCastTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
